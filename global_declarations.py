f12d160936bd0032b571c63742108714ad6d0923
Itamar Hoshmand <hoshmy@gmail.com>
2018-02-16 23:48:21 +0200

    set window location and minor cosmetics

--- global_declarations.py
+++ global_declarations.py
@@ -18,7 +18,7 @@ class EntityType(Enum):
 
 class General(Enum):
     WINDOW_HEIGHT = 300
-    WINDOW_WIDTH = 1280
+    WINDOW_WIDTH = 1000
     MAX_WIDGETS_COLUMN_HEIGHT = 2
     UDP_IP = "127.0.0.1"
     UDP_PORT = 5005
--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import sys
 
 from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QDesktopWidget
 from PyQt5.QtWidgets import QMainWindow
 from PyQt5.QtCore import QSize
 
 from folders_based_tab_widget import FoldersBasedTabWidget
 from global_declarations import General
 
-class HelloWindow(QMainWindow):
+
+class MainWindow(QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
 
@@ -17,10 +19,20 @@ class HelloWindow(QMainWindow):
 
         tabs = FoldersBasedTabWidget(tabs_folder='tabs', parent=self)
         self.setCentralWidget(tabs)
+        self._location_on_the_screen()
+
+    def _location_on_the_screen(self):
+        ag = QDesktopWidget().availableGeometry()
+        sg = QDesktopWidget().screenGeometry()
+
+        widget = self.geometry()
+        x = 0
+        y = ag.height() - General.WINDOW_HEIGHT.value
+        self.move(x, y)
 
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
-    mainWin = HelloWindow()
+    mainWin = MainWindow()
     mainWin.show()
     sys.exit(app.exec_())
